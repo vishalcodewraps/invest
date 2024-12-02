38aa2776b466eb7c5e2d2f81ba0f9558134f679a
Vishal <vishal.mishra.codewraps@gmail.com>
2024-12-02 11:44:01 +0530

    changes

--- resources/views/frontend/maintenance.blade.php
+++ resources/views/frontend/maintenance.blade.php
@@ -1,10 +1,6 @@
 @extends('frontend.layouts.main-layout')
 
-@section('seo')
-    <title>{{ $seo_setting->seo_title }}</title>
-    <meta name="description" content="{!! strip_tags(clean($seo_setting->seo_description)) !!}">
-    <meta name="keywords" content="{{ $seo_setting->seo_keyword }}">
-@endsection
+
 
 @section('content')
     <!-- section start -->
